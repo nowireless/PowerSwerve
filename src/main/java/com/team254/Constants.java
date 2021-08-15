bcafdf5fd71f4668e71da282b5a844fa38c097af
Ryan Sjostrand <ryansjostrand@gmail.com>
2021-08-15 12:42:38 -0500

    Checin of Team 254 Lib

---
+++ src/main/java/com/team254/Constants.java
@@ -0,0 +1,7 @@
+package com.team254;
+
+public class Constants {
+
+    public static final double kLooperDt = 0.01;
+
+}
---
+++ src/main/java/com/team254/lib/subsystems/SubsystemManager.java
@@ -0,0 +1,101 @@
+package com.team254.lib.subsystems;
+
+
+import com.team254.lib.loops.ILooper;
+import com.team254.lib.loops.Loop;
+import com.team254.lib.loops.Looper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Used to reset, start, stop, and update all subsystem1323 at once
+ */
+public class SubsystemManager implements ILooper {
+
+    private final List<CheesySubsystem> mAllSubsystems;
+    private List<Loop> mLoops = new ArrayList<>();
+
+    public SubsystemManager(List<CheesySubsystem> allSubsystems) {
+        mAllSubsystems = allSubsystems;
+    }
+
+    public void outputToSmartDashboard() {
+        mAllSubsystems.forEach((s) -> s.outputTelemetry());
+    }
+
+    public void writeToLog() {
+        mAllSubsystems.forEach((s) -> s.writeToLog());
+    }
+
+    public void stop() {
+        mAllSubsystems.forEach((s) -> s.stop());
+    }
+
+    private class EnabledLoop implements Loop {
+
+        @Override
+        public void onStart(double timestamp) {
+            for (Loop l : mLoops) {
+                l.onStart(timestamp);
+            }
+        }
+
+        @Override
+        public void onLoop(double timestamp) {
+            for (CheesySubsystem s : mAllSubsystems) {
+                s.readPeriodicInputs();
+            }
+            for (Loop l : mLoops) {
+                l.onLoop(timestamp);
+            }
+            for (CheesySubsystem s : mAllSubsystems) {
+                s.writePeriodicOutputs();
+            }
+        }
+
+        @Override
+        public void onStop(double timestamp) {
+            for (Loop l : mLoops) {
+                l.onStop(timestamp);
+            }
+        }
+    }
+
+    private class DisabledLoop implements Loop {
+
+        @Override
+        public void onStart(double timestamp) {
+
+        }
+
+        @Override
+        public void onLoop(double timestamp) {
+            for (CheesySubsystem s : mAllSubsystems) {
+                s.readPeriodicInputs();
+            }
+            for (CheesySubsystem s : mAllSubsystems) {
+                s.writePeriodicOutputs();
+            }
+        }
+
+        @Override
+        public void onStop(double timestamp) {
+
+        }
+    }
+
+    public void registerEnabledLoops(Looper enabledLooper) {
+        mAllSubsystems.forEach((s) -> s.registerEnabledLoops(this));
+        enabledLooper.register(new EnabledLoop());
+    }
+
+    public void registerDisabledLoops(Looper disabledLooper) {
+        disabledLooper.register(new DisabledLoop());
+    }
+
+    @Override
+    public void register(Loop loop) {
+        mLoops.add(loop);
+    }
+}
